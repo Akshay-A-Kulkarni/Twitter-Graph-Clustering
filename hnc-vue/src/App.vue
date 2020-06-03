19d0606b65fc443d85c26d14eea12154b2287398
Akshay Kulkarni <akshayavinashkulkarni.gmail.com>
2020-06-03 18:09:33 -0400

    Refactored old code and cleaned up comments

--- hnc-vue/src/App.vue
+++ hnc-vue/src/App.vue
@@ -68,76 +68,12 @@
                 </div>
                 <br><br>
                 <div class="section has-text-left" id="arch">
-                        <!-- calling imported component from Algo.vue -->
+                        <!-- calling imported component from Arch.vue -->
                         <Architecture />                    
                 </div>
                  <div class="section has-text-left" id="CD">
-                        <!-- calling imported component from Algo.vue -->
+                        <!-- calling imported component from Graph.vue -->
                         <Graph3D />                    
-                 <!--   <h2 class="subtitle is-2">Community Detection</h2><br>
-                    <p class="title is-4">🦠 - Network </p>
-                    <div class="subtitle is-6">
-                        <ul type="circle">
-                         <li>This graph was generated from 10,000+ tweets using #COVID19 as the seed hashtag</li>
-                        </ul>
-                    </div>
-                    <br>
-                    <div class="columns tool-info is-gapless is-vcentered">
-                        <div class="column is-9 is-vcentered">
-                            <div class="container" ref="cd_graph"></div>
-                        </div>
-                        <div class="column is-4 is-vcentered is-offset-2">
-                            <div style="height:100%;">
-                                <p>&nbsp;
-                                    <br><br><br>
-                                    <br><br><br>
-                                    <br><br><br>
-                                    <br><br><br>
-                                    <br><br><br>
-                                    <br><br><br>
-                                    <br><br><br>
-                                    <br><br><br>
-                                    <br><br><br>
-                                </p>
-                            </div>
-                            <div class="content">
-                                <div class="card grow">
-                                <div class="card-content">
-                                    <p class="title is-5">
-                                        3D Force Graph 
-                                        <br>
-                                        <br>
-                                    </p>
-                                    <div class="subtitle is-7">
-                                        <ul type="circle">
-                                            <li> Degree contrast can be observed by the node sizes  </li>
-                                            <li> Node and edge Community membership is represented by colors  </li>
-                                            <li> Additional information about the nodes can be accessed by hovering over nodes. </li>
-                                        </ul>
-                                    <div>
-                                        <p>Inter Community Distance</p>
-                                         <vue-slider 
-                                            v-model="interCommunityDistance"
-                                            @change="updateLinkForce()"
-                                            :min="0"
-                                            :max="10000"
-                                            :interval="1"/>
-                                         <br>
-                                         <p>Intra Community Distance</p>
-                                         <vue-slider 
-                                            v-model="intraCommunityDistance"
-                                            @change="updateLinkForce()"
-                                            :min="-100"
-                                            :max="2000"
-                                            :interval="1"/>
-                                    </div>
-
-                                    </div>
-                                  </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>-->
                 </div> 
             </div>
             </div>
